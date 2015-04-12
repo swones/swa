cfe55b78ab87b50b20d9e2a9b0adf3f79e2b7255
Carlos Morán <cgmoran@gmail.com>
2015-04-12 20:38:39 -0300

    Se agrega vista de ejemplo con url para el home

--- swa/swa/urls.py
+++ swa/swa/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'swa.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from web import urls
 
+
+urlpatterns = [
+    url(r'^web/', include(urls)),
+    url(r'^$', 'web.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
